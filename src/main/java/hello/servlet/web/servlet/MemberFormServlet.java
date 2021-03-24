daa8fe31d3c6433cda7e1aa4d2a9bda366f116fd
keymye <57088536+keymye@users.noreply.github.com>
2021-03-24 16:37:28 +0900

    MVC프레임워크 만들기

--- src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
+++ src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
@@ -22,7 +22,7 @@ public class MemberFormServlet extends HttpServlet {
         response.setCharacterEncoding("utf-8");
 
         PrintWriter w = response.getWriter();
-        w.write("<!DOCTYPE html>\n" + "<html>\n" + "<head>\n" + " <meta charset=\"UTF-8\">\n" +
+        w.write("<!DOCTYPE html>\n" + "<html>\n" + "<head>\n" + " <meta c  harset=\"UTF-8\">\n" +
                 " <title>Title</title>\n" + "</head>\n" + "<body>\n" + "<form action=\"/servlet/members/save\" method=\"post\">\n" + " username: <input type=\"text\" name=\"username\" />\n" + " age: <input type=\"text\" name=\"age\" />\n" + " <button type=\"submit\">전송</button>\n" + "</form>\n" + "</body>\n" + "</html>\n");
     }
 }
